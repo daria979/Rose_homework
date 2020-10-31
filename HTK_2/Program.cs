09a452f6cd44b08fb296625a2338b5100476a125
Trip Daria <daria.trip001@gmail.com>
2020-11-01 01:50:50 +0200

    time

--- HTK_2/Program.cs
+++ HTK_2/Program.cs
@@ -37,10 +37,18 @@ namespace HTK_2
             int n, m, e, b;
             int elevi, banci;
 
-            n = int.Parse(Console.ReadLine());
-            m = int.Parse(Console.ReadLine());
-            e = int.Parse(Console.ReadLine());
-            b = int.Parse(Console.ReadLine());
+            string line;
+
+            line = Console.ReadLine();
+            
+
+            char[] seps = { ' ', '.', ',', '?' };
+            string[] date = line.Split(seps, StringSplitOptions.RemoveEmptyEntries);
+
+            n = int.Parse(date[0]);
+            m = int.Parse(date[1]);
+            e = int.Parse(date[2]);
+            b = int.Parse(date[3]);
             //se deduce faptul ca n<=m
             // se efectueaza un sistem:
             // elevi / n = banci rest e => elevi = n*banci + e
@@ -50,10 +58,61 @@ namespace HTK_2
             banci = (e + m * b) / (m - n);
             elevi = n * banci + e;
 
+            Console.WriteLine($"Numarul de banci: {banci}");
+            Console.WriteLine($"Numarul de elevi: {elevi}");
+
+        }
+
+
+        /// <summary>
+        /// Problema 2
+        /// </summary>
+        /// <param name="args"></param>
+
+        //Se dau 2 numere naturale h m, reprezentând un ora curentă exprimată în ore
+        //şi minute.Să se determine care va fi ora peste x minute.
+
+        //Date de intrare
+        //Programul citește de la tastatură numerele h1 m1 x.
+
+        //Date de ieşire
+        //Programul afișează pe ecran numerele h2 m2, separate printr-un spaţiu, 
+        //cu semnificaţia din enunţ.
+
+        //Restricţii şi precizări
+        //0 ≤ h1, h2 ≤ 23
+        //0 ≤ m1, m2 ≤ 59
+        //0 ≤ x ≤ 10000
+
+        static void time()
+        {
+            int h1, h2, m1, m2, x;
+            string line;
+
+            line = Console.ReadLine();
+
+            char[] seps = { ' ', '.', ',', '?' };
+            string[] time = line.Split(seps, StringSplitOptions.RemoveEmptyEntries);
+
+            h1 = int.Parse(time[0]);
+            m1 = int.Parse(time[1]);
+            x = int.Parse(time[2]);
+
+            
+            Console.WriteLine($"Ora curenta: ora {h1} si {m1} minute");
+
+            m2 = (m1 + x) % 60; 
+            h2 = h1 + x / 60;
+            
+
+            Console.WriteLine($"Ora noua peste {x} minute: ora {h2} si {m2} minute");
+
+
         }
         static void Main(string[] args)
         {
-            elevi();
+            //elevi();
+            time();
         }
     }
 }
